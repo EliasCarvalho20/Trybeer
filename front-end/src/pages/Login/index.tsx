8df7527f58e472bba886c4050a30e4e0c28de1f0
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-03-24 21:32:48 -0300

    Setting up project: React and TypeScript

---
+++ front-end/src/pages/Login/index.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const Login: FC = () => <h1>Hello Friend</h1>;
+
+export default Login;
---
+++ front-end/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Login from '../pages/Login';
+
+const Routes: FC = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/">
+      <Redirect to="/login" />
+    </Route>
+  </Switch>
+);
+
+export default Routes;
